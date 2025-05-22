6afa52100d98e6487d60602e756130ed646d74c4
Rafael Oliveira <luiz.rafael@aluno.ifsp.edu.br>
2025-05-22 16:04:01 -0300

    mesca

--- calculadora.c
+++ calculadora.c
@@ -1,32 +1,70 @@
 #include <stdio.h>
 
-void main(){
-    float x, y;
-    char soma, sub, mult, div, sair;
+int add(int a, int b) {
+    return a + b;
+}
 
-    printf("Digite o valor de x: ");
-    scanf("%f", &x);
+int sub(int a, int b) {
+    return a - b;
+}
 
-    printf("Digite o valor de y: ");
-    scanf("f", &y);
+int mul(int a, int b) {
+    return a * b;
+}
 
-    switch case 'a':
-        printf("Operação soma");
-        soma = x + y;
-        break;
-    case 'b':
-        printf("Operação subtração");
-        sub = x - y;
-        break;
-    case 'c':
-        printf("Operação multiplicação");
-        mult = x * y;
-        break;
-    case 'd':
-        printf("Operação divisão");
-        div = x / y;
-        break;
-    default:
-        printf("Sair do menu")
-        break;
+float div(int a, int b) {
+    return ((float) a)/b;
+}
+
+void ler_valores(int *a, int *b) {
+    printf("Insira os valores (a e b): ");
+    scanf("%d %d", a, b);
+}
+
+int menu() {
+    int op;
+
+    printf("----MENU----\n");
+    printf("1. Adicao;\n");
+    printf("2. Subtracao;\n");
+    printf("3. Multiplicacao;\n");
+    printf("4. Divisao;\n");
+    printf("0. Sair");
+    printf("Selecione: ");
+    scanf("%d", &op);
+    printf("\n");
+
+    return op;
+}
+
+int main(void) {
+    int a, b, op;
+
+    do {
+	op = menu();
+	
+	switch (op) {
+	case 0:
+	    printf("Fim do programa.\n");
+	    break;
+	case 1:
+	    ler_valores(&a, &b);
+	    printf("Soma: %d\n", add(a, b));
+	    break;
+	case 2:
+	    ler_valores(&a, &b);
+	    printf("Subtracao: %d\n", sub(a, b));
+	    break;
+	case 3:
+	    ler_valores(&a, &b);
+	    printf("Produto: %d\n", mul(a, b));
+	    break;
+	case 4:
+	    ler_valores(&a, &b);
+	    printf("Resultado: %.2f\n", div(a, b));
+	    break;
+	default:
+	    printf("Informe uma opcao valida!\n");
+	}
+    } while(op != 0);
 }
